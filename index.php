5ba49042db13895bab5d4148392d6a884a7d1e95
lab1pc6 <lab1pc6@aptech.com>
2023-03-01 17:42:38 +0500

    Index file added!

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>index file</h1>
+
+
+
+    <?php
+
+
+
+
+    // $array = ["name", "email", "address", "password", "number"];
+    // echo $array[0];
+    // echo "<br>";
+    // echo $array[1];
+    // echo $array[2];
+    // echo $array[3];
+    // echo $array[4];
+
+$email = "some@gmail.com";
+$password = "12345";
+
+if ($email == "someone@gmail.com" && $password == 12345) {
+   echo "welcome";
+} else {
+    echo "getlost";
+}
+
+
+
+    ?>
+
+</body>
+
+</html>
